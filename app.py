590cbb3323235e505b52ccc68c30e6147e1464bf
Zabanaa <karim.cheurfi@gmail.com>
2016-09-16 09:36:09 +0000

    Send static index

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app.config.from_pyfile('config.cfg')
 
 @app.route('/')
 def index():
-    return "What's going on bruv"
-
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
