115a54e02e633d5a9274d5a7f0cb37e9ff0969c6
Zabanaa <karim.cheurfi@gmail.com>
2016-09-19 09:30:32 +0000

    Create company model

--- models.py
+++ models.py
@@ -1,19 +1,27 @@
 from app import db
 
-# Create Models here
+class Company(db.Model):
+    id          = db.Column(db.Integer, primary_key=True)
+    email       = db.Column(db.String(100), nullable=False, unique=True)
+    password    = db.Column(db.String(32), nullable=False)
+    name        = db.Column(db.String(40), nullable=False, unique=True)
+    location    = db.Column(db.String(50), nullable=True)
+    website     = db.Column(db.String(60), nullable=True)
+    twitter     = db.Column(db.String(60), nullable=True)
+    facebook    = db.Column(db.String(120), nullable=True)
+    linkedin    = db.Column(db.String(180), nullable=True)
+    bio         = db.Column(db.Text(), nullable=True)
 
-class User(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False)
-    email = db.Column(db.String(40), nullable=False)
-    password = db.Column(db.String(18), nullable=False)
-
-
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
+    def __init__(self, email, password, name, location, website, twitter, facebook, linkedin, bio):
+        self.email      = email
+        self.password   = password
+        self.name       = name
+        self.location   = location
+        self.website    = website
+        self.twitter    = twitter
+        self.facebook   = facebook
+        self.linkedin   = linkedin
+        self.bio        = bio
 
     def __repr__(self):
-       return "User %s" % (self.username) 
+       return "Company %s" % (self.name)
