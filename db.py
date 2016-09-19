ea771b43096b53d8907080b63eaf1fed4222663d
Zabanaa <karim.cheurfi@gmail.com>
2016-09-19 09:30:10 +0000

    Create migration script

--- db.py
+++ db.py
@@ -1,5 +1,14 @@
-from app import db
-from models import User
+from app import app, db
+import models
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-db.create_all()
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('do', MigrateCommand)
+
+if __name__ == "__main__":
+    manager.run()
+
+
 
