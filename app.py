e3d3878001807e69058715d8fc78f973182a3175
Zabanaa <karim.cheurfi@gmail.com>
2016-09-20 10:31:56 +0000

    Serialise JSON payload for register endoint

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import models
+from helpers import send_error
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
@@ -9,6 +11,24 @@ db = SQLAlchemy(app)
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    payload = request.json
+    d = payload.get
+    if payload:
+        try:
+            new_company = models.Company(d('email'), d('password'), d('name'), d('location'), d('website'), d('twitter'),\
+                                     d('facebook'), d('linkedin'), d('bio'))
+            db.session.add(new_company)
+            db.session.commit()
+            return jsonify(new_company.serialise()), 201
+        except Exception as e:
+            if "violates unique constraint" in str(e.__dict__['orig']):
+                return send_error(400, "A company is already registered using this email") 
+
+    return "<h1> Registration Page Bruuuuv </h1>"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
