5fca8511df37cf7a3c3813037776966b6e1f63d1
Zabanaa <karim.cheurfi@gmail.com>
2016-09-21 17:42:00 +0000

    Fix import issue

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
-import models
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db
+from models import Company
 
 migrate = Migrate(app, db)
+
+
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
