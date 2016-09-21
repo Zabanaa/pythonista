2aa69677128f578f9434d1ac2ae5d02f995c240f
Zabanaa <karim.cheurfi@gmail.com>
2016-09-21 09:29:03 +0000

    change password

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class BaseConfig(object):
     DEBUG   = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:msnk2393!.@52.30.100.105/pythonista"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:F**T]nE{M`a9%kK\.@52.30.100.105/pythonista"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class DevelopmentConfig(BaseConfig):
@@ -11,4 +11,4 @@ class DevelopmentConfig(BaseConfig):
 class TestingConfig(BaseConfig):
     DEBUG = False
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:msnk2393!.@52.30.100.105/pythonista_tesing"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:F**T]nE{M`a9%kK\.@52.30.100.105/pythonista_tesing"
