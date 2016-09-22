8151ec9c7d8f02d3a3e218c6cb96c35fc6fe26f0
Zabanaa <karim.cheurfi@gmail.com>
2016-09-22 10:55:18 +0000

    handle not null constraint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from helpers import send_error
+from sqlalchemy.exc import IntegrityError
+from helpers import send_error, get_missing_fields
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
@@ -18,17 +19,22 @@ def register():
     if request.method == "POST":
         payload = request.json
         d = payload.get
-
         try:
             new_company = Company(d('email'), d('password'), d('name'), d('location'), d('website'), d('twitter'),\
                                                                             d('facebook'), d('linkedin'), d('bio'))
             db.session.add(new_company)
             db.session.commit()
             return jsonify(new_company.serialise()), 201
-
-        except Exception as e:
-            if "violates unique constraint" in str(e.__dict__['orig']):
-                return jsonify({"code": 400, "message": "A company is already registered using this email"})
+        except IntegrityError as e:
+            cause_of_error = str(e.__dict__['orig'])
+            if "violates unique constraint" in cause_of_error:
+                return jsonify({"code": 400, "message": "A company is already registered using this email"}), 400
+            elif "not-null" in cause_of_error:
+                missing_fields = get_missing_fields(e.__dict__['params'])
+                return jsonify({"code": "400", "missing_fields": missing_fields, "message": "wagwaan bruv, you missed\
+                some fields", "reason": cause_of_error}), 400
+            else:
+                return jsonify({"code": "400", "message": cause_or_error}), 400
     else:
         return "Whuuuuh ? Just let man register"
 
