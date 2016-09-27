a8a87429161d14261e43e795293a9fd585660f18
= <=>
2016-09-27 10:02:45 +0000

    extend the character limit of the password field

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from app import db
 class Company(db.Model):
     id          = db.Column(db.Integer, primary_key=True)
     email       = db.Column(db.String(100), nullable=False, unique=True)
-    password    = db.Column(db.String(32), nullable=False)
+    password    = db.Column(db.String(64), nullable=False)
     name        = db.Column(db.String(40), nullable=False)
     location    = db.Column(db.String(50), nullable=True)
     website     = db.Column(db.String(60), nullable=True)
