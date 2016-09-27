e69696dfa2f862a916196d445d643c027a260864
= <=>
2016-09-27 08:42:49 +0000

    Refactor send_error function

--- helpers.py
+++ helpers.py
@@ -1,11 +1,22 @@
 from flask import jsonify
 
-def send_error(code, message):
-    error = {
-        'code': code,
-        'message': message
-    }
-    return jsonify(error)
+def send_error(status_code, message=None, missing_fields=None):
+
+    if missing_fields is not None:
+
+        error = {
+            'status_code': status_code,
+            'message': "Incomplete request. Missing required fields",
+            'missing_fields': missing_fields,
+        }
+
+    else:
+        error = {
+            'status_code': status_code,
+            'message': message
+        }
+
+    return jsonify(error), status_code
 
 def get_missing_fields(response_body):
     missing_fields = []
