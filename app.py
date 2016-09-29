2774544abcfe95c7020a444ff958625d3370ed47
= <=>
2016-09-29 08:58:18 +0000

    Split post and get methods into two functions for /login

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ from models import *
 
 @app.route('/')
 def index():
-    return "hello this is the index page newwww one brruuuv", 200
+    return "Hello and welcome to pythonista.io", 200
 
 @app.route('/register', methods=['GET'])
 def load_register_page():
-    return "Whuuuuh ? Just let man register", 200
+    return "Plase fill out the form to register an account for your company", 200
 
 @app.route("/register", methods=['POST'])
 def register_user():
@@ -43,29 +43,33 @@ def register_user():
     except IntegrityError as e:
         cause_of_error = str(e.__dict__['orig'])
         if "violates unique constraint" in cause_of_error:
-            return send_error(409, "Bruv, A company is already registered using this email")
+            return send_error(409, "sorry, it seems like a company is already registered using this email")
         elif "not-null" in cause_of_error:
             missing_fields = get_missing_fields(e.__dict__['params'])
             return send_error(409, missing_fields=missing_fields)
         else:
             return jsonify({"status_code": 400, "message": cause_or_error}), 400
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/login', methods=['GET'])
+def load_login_page():
+    return "Hello, please submit your credentials to log in", 200
+
+
+@app.route('/login', methods=["POST"])
 def login():
 
-    if request.method == 'POST':
-        login_credentials = request.get_json()
-        value = login_credentials.get
-        email = value('email')
-        password = value('password')
-        company = Company.query.filter_by(email=email).first()
+    login_credentials = request.get_json()
+    value = login_credentials.get
+    email = value('email')
+    password = value('password')
+    company = Company.query.filter_by(email=email).first()
 
-        if check_password_hash(company.password, password) == True:
-            return jsonify({"message": "you are logged in", "status_code": 200}), 200
-        else:
-            return send_error(401, "Sorry, the password you provided is incorrect") 
+    if check_password_hash(company.password, password) == True:
+        return jsonify({"message": "you are logged in", "status_code": 200}), 200
+    else:
+        return send_error(401, "Sorry, the password you provided is incorrect")
 
-        # if passwords match
+    # if passwords match
             # set the session
             # log the user in
             # redirect them to the home page
@@ -73,10 +77,7 @@ def login():
         # else return a 401 unauthorised
         # with a message saying wrong password
 
-        return jsonify(login_credentials)
-
-    else:
-        return 'Bruv you want to login ?', 200
+    return jsonify(login_credentials)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
