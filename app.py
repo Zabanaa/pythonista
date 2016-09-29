c170d6ecd5f5a52a6572873a78a0a578b3b17a89
= <=>
2016-09-29 10:25:17 +0000

    Check if company exists and create session upon successful login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from helpers import send_json, get_missing_fields, hash_user_password
@@ -64,20 +64,26 @@ def login():
     password = value('password')
     company = Company.query.filter_by(email=email).first()
 
-    if check_password_hash(company.password, password) == True:
-        return send_json(200, "you are logged in")
+    # Check if the email corresponds to a company in the db
+        # if so,
+            # check the password they provided with the password hash related to that company
+            # if they match
+                # they get a 200 ok
+                # and a redirection url to use on the front end
+                # also, we set the session for that user
+            #else
+                # return a 401 with a message about the passwd
+    # else
+        # we return a 401 unauthorised because the password they provided did not match the hash in the db
+
+    if company is not None:
+        if check_password_hash(company.password, password) == True:
+            session['company'] = email
+            return send_json(200, "you are logged in") # send a redirection url along with the message
+        else:
+            return send_json(401, "Sorry, the password you provided is incorrect")
     else:
-        return send_json(401, "Sorry, the password you provided is incorrect")
-
-    # if passwords match
-            # set the session
-            # log the user in
-            # redirect them to the home page
-
-        # else return a 401 unauthorised
-        # with a message saying wrong password
-
-    return send_json(200, login_credentials)
+        return send_json(401, "The company does not exist bruv whuh you doin ?")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
