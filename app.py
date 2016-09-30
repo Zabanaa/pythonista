211d4858ed069ec6509c3ad21dcec036f90118bf
= <=>
2016-09-30 11:04:58 +0000

    replace send_json with send_response

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config.from_object('config.DevelopmentConfig')
 
 db = SQLAlchemy(app)
 from models import *
-from helpers import send_json, get_missing_fields, hash_user_password, register_company
+from helpers import get_missing_fields, hash_user_password, register_company, send_response
 
 
 @app.route('/')
@@ -29,16 +29,31 @@ def register_user():
     form = request.get_json()
     try:
         new_company = register_company(form)
-        return send_json(201, resource=new_company.serialise())
+        return send_response(201, {
+            "status_code" : 201,
+            "resource": new_company.serialise()
+        })
     except IntegrityError as e:
         cause_of_error = str(e.__dict__['orig'])
         if "violates unique constraint" in cause_of_error:
-            return send_json(409, "sorry, it seems like a company is already registered using this email")
+            return send_response(409,{
+                "status_code": 409,
+                "message": "sorry, it seems like a company is already registered using this\
+                email"
+            })
+
         elif "not-null" in cause_of_error:
             missing_fields = get_missing_fields(e.__dict__['params'])
-            return send_json(409, missing_fields=missing_fields)
+            return send_response(409, {
+                "status_code": 409,
+                "message" : "Incomplete request. Missing required fields.",
+                "missing_fields" : missing_fields
+            })
         else:
-            return send_json(400, cause_or_error)
+            return send_response(400, {
+                "status_code": 400,
+                "message": cause_of_error
+            })
 
 @app.route('/login', methods=['GET'])
 def load_login_page():
@@ -57,12 +72,21 @@ def login():
     if company is not None:
         if check_password_hash(company.password, password) == True:
             session['company'] = company.name
-            welcome_message = "Hello %s, welcome back" % session['company']
-            return send_json(200, welcome_message) # Add redirect url
+            return send_response(200, {
+                "status_code": 200,
+                "message": "Hello %s, welcome back" % session['company'],
+                "redirect_url": "/"
+            })
         else:
-            return send_json(401, "Sorry, the password you provided is incorrect")
+            return send_response(401, {
+                "status_code": 401,
+                "message":"Sorry, the password you provided is incorrect"
+            })
     else:
-        return send_json(401, "Sorry, there is no company registered at this address")
+        return send_response(401, {
+            "status_code": 401,
+            "message": "Sorry, there is no company registered at this address"
+        })
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
