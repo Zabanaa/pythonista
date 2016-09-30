d3df06fd474e9042f3dafeab91a2083eb5974394
= <=>
2016-09-30 08:06:50 +0000

    Add docs to helper function

--- helpers.py
+++ helpers.py
@@ -2,11 +2,21 @@ from flask import jsonify
 from werkzeug.security import generate_password_hash
 
 def hash_user_password(password):
+
+    '''
+        Takes a plain text password and encrypts it
+    '''
+
     if password is not None:
         hashed_password = generate_password_hash(password)
         return hashed_password
 
 def send_json(status_code, message=None, resource=None, missing_fields=None):
+
+    '''
+        Takes a dictionary containing the response object to return to the user
+        in json format
+    '''
 
     if missing_fields is not None:
 
@@ -31,6 +41,14 @@ def send_json(status_code, message=None, resource=None, missing_fields=None):
     return jsonify(response), status_code
 
 def get_missing_fields(response_body):
+
+    '''
+    Accepts a dict containing the payload.
+    Loops through that dict
+    Appends the keys with a value of None to a list
+    returns that list
+    '''
+
     missing_fields = []
     for key, value in response_body.items():
         if response_body[key] == None:
