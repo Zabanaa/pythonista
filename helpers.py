dec1c020fab92a95868c88edc563c59356d5473b
= <=>
2016-10-03 08:14:22 +0000

    remove password check from register company helper

--- helpers.py
+++ helpers.py
@@ -11,10 +11,10 @@ from werkzeug.security import generate_password_hash
 # else return a 403 unauthorised and redirect the user
 
 def wrong_password():
-    return jsonify({"error": "The password you provided is incorrect", "status_code": 401)
+    return jsonify({"error": "The password you provided is incorrect", "status_code": 401})
 
 def email_already_registered():
-    return jsonify({"error": "A company is already registered using this email", "status_code", 409})
+    return jsonify({"error": "A company is already registered using this email", "status_code": 409})
 
 def wrong_email():
     return jsonify({"error": "No company is registered using this email address", "status_code": 401})
@@ -43,9 +43,6 @@ def register_company(company_obj):
     Accepts a request dictionary and saves it as a Company 
     instance in the database
     '''
-
-    if 'password' in company_obj:
-        company_obj['password'] = hash_user_password(company_obj['password'])
 
     new_company = Company(company_obj)
     db.session.add(new_company)
