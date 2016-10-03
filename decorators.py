ec12148f7307f1e20aae2bedcaceac46a0e8807a
= <=>
2016-10-03 09:17:08 +0000

    Add decorators file

---
+++ decorators.py
@@ -0,0 +1,8 @@
+# Login required decorator
+# check if company is in the session
+# check that the session value is equal to the actual company name
+# if so execute the function
+# else return a 403 unauthorised and redirect the user
+
+
+
