5469ce707b6c11a0cd5a9519bc8c598e0591412a
= <=>
2016-10-03 10:09:18 +0000

    Create json decorator

--- decorators.py
+++ decorators.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 # Login required decorator
 # check if company is in the session
 # check that the session value is equal to the actual company name
@@ -5,4 +6,19 @@
 # else return a 403 unauthorised and redirect the user
 
 
+# Serialise Json
+
+def serialise_json(f):
+    '''
+
+    Takes the object returned by the function called 
+    and jsonifies it
+
+    '''
+    def wrapper():
+        return_value = f()
+        return_value = jsonify(return_value)
+        return return_value
+
+    return wrapper
 
