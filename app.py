3e37e4871adfe6667a294d152181de6d99455fb8
= <=>
2016-10-06 08:02:40 +0000

    Protect post_job and create get_job_by_id endpoint

--- app.py
+++ app.py
@@ -49,12 +49,23 @@ def companies():
 def company(company_id):
     return get_company(company_id)
 
-
 @app.route('/api/jobs', methods=['POST'])
 @serialise_json
+@login_required
 def post_job():
     form = request.get_json()
     return publish_job(form)
+
+@app.route('/api/jobs/<int:job_id>', methods=['GET'])
+@serialise_json
+@login_required
+def get_job_by_id(job_id):
+    return get_job(job_id)
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
