650419659f17401517d9d04cb481b4ea09c82e7d
= <=>
2016-10-18 09:22:20 +0000

    change config initiation and create mail object

--- pythonista/__init__.py
+++ pythonista/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+app.config.from_object('pythonista.config.DevelopmentConfig')
+mail = Mail(app)
 
 from pythonista.api.routes import api
 from pythonista.auth.routes import auth
-from pythonista.config import DevelopmentConfig
-
 
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(auth)
 
-app.config.from_object(DevelopmentConfig)
 
 @app.route('/')
 def index():
