08968e0d71e29f06f261887f20bd3e1abd2f079d
= <=>
2016-10-19 20:34:46 +0000

    Add comments to the decorators

--- pythonista/decorators.py
+++ pythonista/decorators.py
@@ -4,6 +4,14 @@ from .models import *
 import functools
 
 def login_required(f):
+
+    '''
+        Checks if the session contains a key called company
+        And that it matches the email of the actual company
+        If it does, it proceeds to the function called by the controller
+        else it returns a 403 Unauthorised response
+    '''
+
     @functools.wraps(f)
     def wrapped(*args, **kwargs):
         if 'company' in session:
@@ -12,22 +20,16 @@ def login_required(f):
             if session['company'] == company.email:
                 return f(*args, **kwargs)
         else:
-            return unauthorised() 
+            return unauthorised()
     return wrapped
-
-# check if company is in the session
-# check that the session value is equal to the actual company name
-# if so execute the function
-# else return a 403 unauthorised and redirect the user
-
 
 # Serialise Json
 def serialise_json(f):
     '''
-
-    Takes the object returned by the function called 
-    and jsonifies it
-
+        Takes the return value (aka the response) from the
+        controller and jsonifies it
+        (Only works if the response is a tuple)
+        (This decorator is dependent on the order by which the elemnts are returned)
     '''
     @functools.wraps(f)
     def wrapped(*args, **kwargs):
