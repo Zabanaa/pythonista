02b449b49749616d6035970c2af142f475922d5e
= <=>
2016-10-20 08:54:25 +0000

    Comment manage.py functions

--- manage.py
+++ manage.py
@@ -3,16 +3,22 @@ from flask_migrate import Migrate, MigrateCommand
 from pythonista import app, db
 from pythonista.models import Company, Job
 
-migrate = Migrate(app, db, compare_type=True)
-
-
+migrate = Migrate(app, db, compare_type=True) 
 manager = Manager(app)
+
 
 @manager.command
 def test():
+    '''
+        This function will enable the user to run the tests by issuing a single
+        command:
+        python manage.py test
+    '''
     from subprocess import call
     call('python -m tests.test', shell=True)
 
+# This adds the db command to our module
+# We can then do python manage.py db init/migrate/upgrade etc
 manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
