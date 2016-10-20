420ed02f5668e9323da82b19ad4adcc4cecaed54
= <=>
2016-10-20 11:12:57 +0000

    Remove duplicate helpers and comment functions

--- pythonista/auth/helpers.py
+++ pythonista/auth/helpers.py
@@ -4,33 +4,51 @@ from ..models import Company
 from sqlalchemy.exc import IntegrityError
 
 def login_successful(company_email=None):
+    '''
+        Takes an email as a param.
+        Sets the session's company key to the previously passed email
+        returns a 302 along with a full url to the index page in the location header (used for redirection)
+    '''
     session['company'] = company_email
     return 302, {"message": "Login successful"}, {"Location": url_for('index')}
 
 def logout_successful():
+    '''
+        Clears the session
+        Sends a 302 along with a full url to the index page in the location header (used for redirection)
+    '''
     session.clear()
     return 302, {"message": "You have been logged out"}, {"Location": url_for('index')}
 
 def logout_company():
+    ''' calls the above function '''
     return logout_successful()
 
 def wrong_password():
+    '''
+        returns a 401 unauthorised
+    '''
     return 401, {"error": "The password you provided is incorrect", "status_code": 401}, {}
 
 def wrong_email():
+    '''
+        returns a 401 unauthorised if a user attempts to login using a non existant email address
+    '''
     return 401, {"error": "No company is registered using this email address", "status_code": 401}, {}
 
-def incomplete_request(missing_fields=None):
-    return 409, {"error": "Incomplete request, Missing required fields.", "status_code": 409,\
-                 "missing_fields": missing_fields}, {}
-
-def unauthorised():
-    return 403, {"error": "Unauthorised", "status_code": 403}, {"Location": url_for('auth.login')}
-
-def bad_request(reason=None):
-    return 400, {"error": "Something went wrong", "status_code": 400, "reason": reason}, {}
-
 def login_company(payload):
+
+    '''
+        Takes a JSON payload in the form of a dict
+        We extract the email and password
+        Lookup the company by email in the DB
+        If there's a match
+        We check that the password matches the hash of that company
+        We then log them in
+        else we return a 401 error
+        and if the company does not exist we return a wrong email error
+
+    '''
 
     email, password = (payload['email'], payload['password'])
     company = Company.query.filter_by(email=email).first()
@@ -43,18 +61,3 @@ def login_company(payload):
                 return wrong_password()
     else:
         return wrong_email()
-
-def get_missing_fields(response_body):
-
-    '''
-    Accepts a dict containing the payload.
-    Loops through that dict
-    Appends the keys with a value of None to a list
-    returns that list
-    '''
-
-    missing_fields = []
-    for key, value in response_body.items():
-        if response_body[key] == None:
-            missing_fields.append(key)
-    return missing_fields
